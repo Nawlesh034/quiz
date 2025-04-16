756687f42cbd5c59fa3206cb0b00c28fb43250d5
Nawlesh034 <nn802301@gmail.com>
2025-04-16 05:36:38 +0530

    Configured Vercel deployment

--- api/data.js
+++ api/data.js
@@ -1,8 +1,9 @@
+// api/data.js
 const { readFileSync } = require('fs');
 const path = require('path');
 
 module.exports = (req, res) => {
-  const filePath = path.resolve(process.cwd(), 'src/api/api.json'); // ✅ full path to api.json
+  const filePath = path.join(__dirname, 'api.json');
   const jsonData = JSON.parse(readFileSync(filePath, 'utf-8'));
 
   res.setHeader('Content-Type', 'application/json');
