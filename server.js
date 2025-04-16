7a8994d820d2f4696248b68738abfb89ae8a502b
Nawlesh034 <nn802301@gmail.com>
2025-04-16 06:09:29 +0530

    vercel-8

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('api/api.json'); // Adjust the path to your db file
+const router = jsonServer.router('api/api.json'); // Path to your data file
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
+// Handle errors gracefully
+server.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+server.use(router);
+
 // Vercel assigns the port dynamically, so we use process.env.PORT
-server.listen(process.env.PORT || 8000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running');
 });
